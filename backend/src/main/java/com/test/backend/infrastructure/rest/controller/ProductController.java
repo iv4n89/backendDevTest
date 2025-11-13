492760845ad507b9f59251a98dc835e5e77e1009
iv4n89 <ivan.betanzos89@gmail.com>
2025-11-13 23:59:47 +0100

    feat: rest controller

---
+++ backend/src/main/java/com/test/backend/infrastructure/rest/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.test.backend.infrastructure.rest.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.backend.domain.model.ProductDetail;
+import com.test.backend.domain.port.input.GetSimilarProductsUseCase;
+import com.test.backend.infrastructure.dto.ProductResponse;
+import com.test.backend.infrastructure.mapper.ProductRestMapper;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+@Tag(name = "Product Controller", description = "APIs for product management")
+public class ProductController {
+    private final GetSimilarProductsUseCase getSimilarProductsUseCase;
+
+    @GetMapping("/{productId}/similar")
+    public ResponseEntity<List<ProductResponse>> getSimilarProducts(
+        @PathVariable String productId
+    ) {
+        log.info("GET /product/{}/similar called", productId);
+
+        List<ProductDetail> products = getSimilarProductsUseCase.execute(productId);
+        List<ProductResponse> response = ProductRestMapper.toProductResponseList(products);
+
+        return ResponseEntity.ok(response);
+    }
+}
